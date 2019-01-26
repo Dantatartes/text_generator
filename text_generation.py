ce6e341e8ebe90565ec724b4e8343a1fb2fc0431
Danis <danis.ds@yandex.ru>
2019-01-27 01:17:51 +0300

    Make fitting with file, add 'save_model' method

--- text_generation.py
+++ text_generation.py
@@ -1,5 +1,6 @@
 import re
-from numpy.random import randint, choice
+import csv
+from random import randint, choice
 
 
 class TextGenerator:
@@ -8,20 +9,24 @@ class TextGenerator:
         self.__word_dict = {}
         pass
 
-    def fit(self, raw_text):
-
+    def fit(self, file):
         def tokenize(string):
             return list(map(str, re.sub(r'[^a-zA-Zа-яА-Я]+', ' ', string).split()))
 
         def make_lowercase(word_list):
             return [string.lower() for string in word_list]
-
-        m = make_lowercase(tokenize(raw_text))
-        for word, next_word in zip(m, m[1:]):
-            if word not in self.__word_dict.keys():
-                self.__word_dict[word] = [next_word]
-            else:
-                self.__word_dict[word].append(next_word)
+        try:
+            with open(file, 'r') as f:
+                m = f.read()
+                f.close()
+            m = make_lowercase(tokenize(m))
+            for word, next_word in zip(m, m[1:]):
+                if word not in self.__word_dict.keys():
+                    self.__word_dict[word] = [next_word]
+                else:
+                    self.__word_dict[word].append(next_word)
+        except FileNotFoundError as e:
+            print(e)
 
     def generate(self, size=randint(7, 14)):
         try:
@@ -32,13 +37,15 @@ class TextGenerator:
                 sentence = f"{sentence}{new_word} "
                 current_word = new_word
             return sentence
-        except ValueError:
+        except (ValueError, IndexError):
             print("You haven't fitted the data to TextGenerator or it was an empty string")
 
+    def save_model(self):
+        with open("saved_model.csv", 'w') as f:
+            for key in list(self.__word_dict.keys()):
+                f.write(f"{key},{self.__word_dict[key]}\n")
 
-with open('constitution.txt', 'r') as f:
-    raw = f.read()
-    f.close()
+
 generator = TextGenerator()
-generator.fit(raw)
+generator.fit("constitution.txt")
 print(generator.generate())
