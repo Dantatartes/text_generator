320c94ca37a14d93c062551210c4606c248bce1b
Danis <danis.ds@yandex.ru>
2019-01-26 21:44:11 +0300

    Make 'generate' method (no exception handling)

--- text_generation.py
+++ text_generation.py
@@ -1,22 +1,33 @@
 import re
+from numpy.random import randint, choice
 
 
 class TextGenerator:
     word_dict = {}
+    m = []
 
     def __init__(self):
         pass
 
     def fit(self, raw_text):
-        m = make_lowercase(tokenize(raw_text))
-        for word, next_word in zip(m, m[1:]):
+        self.m = make_lowercase(tokenize(raw_text))
+        for word, next_word in zip(self.m, self.m[1:]):
             if word not in self.word_dict.keys():
                 self.word_dict[word] = [next_word]
             else:
                 self.word_dict[word].append(next_word)
 
-    def generate(self):
-        return self.word_dict
+    def generate(self, size=randint(7, 14)):
+        try:
+            current_word = choice(self.word_dict.keys())
+        except ValueError:
+            return ""
+        sentence = f"{current_word} "
+        for i in range(size):
+            new_word = choice(self.word_dict[current_word])
+            sentence = f"{sentence}{new_word} "
+            current_word = new_word
+        return sentence
 
 
 # Избавление от неалфавитных знаков, сплит их в лист
