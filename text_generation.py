bd89f334d64e0a389a2baf1a2916fe03940e3850
Danis <danis.ds@yandex.ru>
2019-01-27 00:42:29 +0300

    Make a word_dict shadow, fix 'tokenize' method to allow cyrillic characters and delete numbers

--- text_generation.py
+++ text_generation.py
@@ -5,23 +5,30 @@ from numpy.random import randint, choice
 class TextGenerator:
 
     def __init__(self):
-        self.word_dict = {}
+        self.__word_dict = {}
         pass
 
     def fit(self, raw_text):
+
+        def tokenize(string):
+            return list(map(str, re.sub(r'[^a-zA-Zа-яА-Я]+', ' ', string).split()))
+
+        def make_lowercase(word_list):
+            return [string.lower() for string in word_list]
+
         m = make_lowercase(tokenize(raw_text))
         for word, next_word in zip(m, m[1:]):
-            if word not in self.word_dict.keys():
-                self.word_dict[word] = [next_word]
+            if word not in self.__word_dict.keys():
+                self.__word_dict[word] = [next_word]
             else:
-                self.word_dict[word].append(next_word)
+                self.__word_dict[word].append(next_word)
 
     def generate(self, size=randint(7, 14)):
         try:
-            current_word = choice(list(self.word_dict.keys()))
+            current_word = choice(list(self.__word_dict.keys()))
             sentence = f"{current_word} "
             for i in range(size):
-                new_word = choice(self.word_dict[current_word])
+                new_word = choice(self.__word_dict[current_word])
                 sentence = f"{sentence}{new_word} "
                 current_word = new_word
             return sentence
@@ -29,17 +36,9 @@ class TextGenerator:
             print("You haven't fitted the data to TextGenerator or it was an empty string")
 
 
-# Избавление от неалфавитных знаков, сплит их в лист
-def tokenize(string):
-    return list(map(str, re.sub(r'[^\w]', ' ', string).split()))
-
-
-#  Перевод всего в нижний регистр
-def make_lowercase(word_list):
-    return [string.lower() for string in word_list]
-
-
-raw = str(input())
+with open('constitution.txt', 'r') as f:
+    raw = f.read()
+    f.close()
 generator = TextGenerator()
 generator.fit(raw)
 print(generator.generate())
